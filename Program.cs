9b7f51dcfda2598d0607e3201ea01f94cd288d97
Alex <pirogav53@gmail.com>
2022-12-14 19:58:58 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] myArray = Auto();
+int[] Auto()
+{
+    int[] myArray;
+    myArray = new int[8];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+
+        System.Console.WriteLine($"Введите значение элемента массива индекса {i}");
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return myArray;
+}
+System.Console.WriteLine(string.Join(" | ", myArray));
